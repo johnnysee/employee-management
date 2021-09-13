6e5fe9bc446521e7661265541c9db632b93f7f93
johnnysee <js.johnny.see@gmail.com>
2021-09-13 22:21:48 +0200

    Added a few it blocks to expect employee-list with length 6 and display expected content

---
+++ cypress/integration/displayEmployeeList.feature.js
@@ -0,0 +1,24 @@
+describe('Display list of employees', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('is expected to display a header', () => {
+    cy.get('#header').should('contain', 'Employee List')
+  })
+
+  it('is expeted to display a list with 6 items', () => {
+    cy.get('#employee-list').within(() => {
+      cy.get('.employee-item').should('have.length', 6)
+    })
+  })
+
+  it('is expected list items to display the expected content', () => {
+    cy.get('#employee-list').within(() => {
+      cy.get('.employee-item')
+      .first()
+      .find('.name')
+      .should('contain', 'George Bluth')
+    })
+  })
+})
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 id="header">Employee List</h1>
+      </div>
+    )
+  }
 }
-
 export default App
